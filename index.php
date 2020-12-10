f2bd71f7d0258fad1b0ee91c1693354e59634d3f
Natalia <gonzalezsolisnm@gmail.com>
2020-12-10 03:30:56 -0300

    Agregué una imagen para icon

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Calculadora PHP</title>
+    <link rel="icon" type="image/png" href="Calculator-Icon.png">
     <link rel="stylesheet" type="text/css" href="styles.css">
 </head>
 <body>
